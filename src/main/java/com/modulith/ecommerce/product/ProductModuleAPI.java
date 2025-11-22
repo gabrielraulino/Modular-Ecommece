89c237d655425206924e00bef67a4708c5fe0aa1
Gabriel Raulino <gabrielviana@alu.ufc.br>
2025-11-22 20:44:51 -0300

    chore: enhance ProductModuleAPI with improved documentation

--- src/main/java/com/modulith/ecommerce/product/ProductModuleAPI.java
+++ src/main/java/com/modulith/ecommerce/product/ProductModuleAPI.java
@@ -2,16 +2,27 @@ package com.modulith.ecommerce.product;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 
 public interface ProductModuleAPI {
 
-    Optional<ProductDTO> findProductById(Long productId);
-
+    /**
+     * Find all products by their IDs
+     * @param productIds Set of product IDs
+     * @return List of ProductDTOs
+     */
     List<ProductDTO> findAllProductsByIds(Set<Long> productIds);
+    
+    /**
+     * Validate product stock
+     * @param productId Product ID
+     * @param requiredQuantity Required quantity
+     */
+    void validateProductStock(Long productId, int requiredQuantity);
 
-    ProductDTO validateProductStock(Long productId, int requiredQuantity);
-
+    /**
+     * Validate products stock
+     * @param productQuantities Map of product IDs and required quantities
+     */
     void validateProductsStock(Map<Long, Integer> productQuantities);
 }
